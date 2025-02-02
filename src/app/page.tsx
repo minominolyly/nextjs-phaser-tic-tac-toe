c811d388c5750ebe6421afbc437eeac94cd285a2
minominolyly <95477322+minominolyly@users.noreply.github.com>
2025-02-02 19:31:57 +0900

    fix resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { GameElement } from "@/components/GameElement";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <GameElement />
-      </main>       
-    </div>
+    <main>
+      <GameElement />
+    </main>
   );
 }
